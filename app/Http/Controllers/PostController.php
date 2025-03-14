1a3d5bfc405c20583b6e984804f882aae8c1bcdc
rookie7150 <ander030517@gmail.com>
2025-03-14 16:02:50 +0800

    test postman

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use app\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    
+    public function store(Request $request){
+
+        $post = Post::create($request->all());
+        return response()->json($post, 201);
+
+    }
+
+    public function index(){
+        $posts = Post::with('user')->get();
+        return response()->json($posts);
+    }
+
+}
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'description', 'image', 'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(App\Models\User::class);
+    }
+
 }
